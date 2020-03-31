beffcacc43184c45a29417a5d4d0f5eb364024ce
blannamon <55991432+blannamon@users.noreply.github.com>
2020-03-31 11:56:22 +0300

    Add files via upload

---
+++ Stage 3/products-catalog/products-cata/js/Product.js
@@ -0,0 +1,40 @@
+let parent = document.getElementById('product-grid');
+class Product {
+   
+    constructor( name, category = "All items", images = [], currency = {"amount": undefined, "currency": undefined}, quantity = 0 ) {
+        this.name = name;
+
+        typeof category === "string" ? this.category = category : console.error('Category must be a string');
+        
+        Array.isArray(images) ? this.images = images : console.error('Images must be an array');
+
+        Object.keys(currency).includes('amount') && Object.keys(currency).includes('currency') ? this.currency = currency : console.error('Currency must be an object containing "currency" and "amount" properties');
+
+        Number.isInteger(quantity) ? this.quantity = quantity : console.error('Quantity must be an integer');
+    }
+
+    render(){
+        let div = create('div', parent.querySelector('.row'), 'col-md');
+        
+        // Upper part - Images
+        let imageContainer = create('div', div, 'img-container owl-carousel');
+        this.images.forEach(
+            image => { 
+                let img = create('img', imageContainer, 'img');
+                img.setAttribute('src', image)
+             }
+        )
+        
+        // Lower part - Information
+        let infoContainer = create('div', div, 'info-container');
+        let h4 = create('h4', infoContainer, undefined, this.name);
+        let h6 = create('h6', infoContainer, undefined, this.category);
+        let qtyContainer = create('div', infoContainer, 'qty-container');
+        let qty = create('p', qtyContainer, 'quantity', this.quantity ? this.quantity : '0');
+        let leftTxt = create('p', qtyContainer, 'txt-qty', 'left');
+        let preloader = create('p', qtyContainer, 'preloader');
+        let priceContainer = create('div', infoContainer, 'price-container');
+
+        return div    
+    }
+}
---
+++ Stage 3/products-catalog/products-cata/js/catalog.js
@@ -0,0 +1,38 @@
+// let images1 = ["https://i.picsum.photos/id/112/400/300.jpg", "https://i.picsum.photos/id/114/400/300.jpg", "https://i.picsum.photos/id/116/400/300.jpg"]
+let images1 = ['js/iphone1.png', 'js/iphone2.png', 'js/iphone3.jpg']
+let currency1 = {
+    amount: 100,
+    currency: "USD"
+}
+let currency2 = {
+    amount: 1300,
+    currency: "EUR"
+}
+let currency3= {
+    amount: 700,
+    currency: "CAD"
+}
+let products = [
+    new Product("iPhone XII", "Mobile phone", images1, currency1, 9),
+    new Product("iMac XIV", "PC", ["https://i.picsum.photos/id/118/400/300.jpg", "https://i.picsum.photos/id/119/400/300.jpg"], currency2, 4),
+    new Product("iPad IX", "Tablet", ["https://i.picsum.photos/id/128/400/300.jpg"], currency3, 3),
+    new Product("Keyboard", undefined, ["https://i.picsum.photos/id/206/400/300.jpg"])
+]
+
+function create(tagName, parentObj, ...rest){
+    let div = document.createElement(tagName);
+    parentObj.appendChild(div);
+    rest[0] ? div.className = rest[0] : null;
+    rest[1] ? div.innerHTML = rest[1] : null;
+    return div;
+}
+
+
+function renderProductsGrid( /*selector*/ ){
+    // let wrapper = document.querySelector( selector )
+        products.forEach(  
+            product => /*wrapper.appendChild(*/ product.render() //)
+        )
+}
+renderProductsGrid()
+
